542c3029912e25b3a037d28b4318dd8663607c3a
FurkanDonertas <furkan6240@gmail.com>
2025-01-01 23:24:24 +0300

    login, signup, forgot password halloldu 23.24/01.01.25

--- app/src/main/java/com/furkandonertas/idealustam/features/auth/presentation/view/LoginActivity.kt
+++ app/src/main/java/com/furkandonertas/idealustam/features/auth/presentation/view/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.furkandonertas.idealustam.features.auth.presentation.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.furkandonertas.idealustam.R
 
@@ -8,5 +10,17 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // Sign Up butonuna tıklandığında SignUpActivity'ye yönlendir
+        findViewById<TextView>(R.id.signUpButton).setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+            finish()
+        }
+
+        // Forgot Password'a tıklandığında ForgotPasswordActivity'ye yönlendir
+        findViewById<TextView>(R.id.forgotPasswordText).setOnClickListener {
+            startActivity(Intent(this, ForgotPasswordActivity::class.java))
+            finish()
+        }
     }
 } 
